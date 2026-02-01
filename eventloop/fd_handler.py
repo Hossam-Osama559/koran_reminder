25d2e5b773ae24a67c078a26496b45e30542fb04
hossam osama <ho4398977@gmail.com>
2026-02-01 04:50:27 +0200

    class for handling the events of fired fd , and the creating fd

--- eventloop/fd_handler.py
+++ eventloop/fd_handler.py
@@ -24,3 +24,62 @@ class timerfd_and_match_mapping:
         return self.mapping[name]
 
 
+class fd_handler:
+
+
+    mapping_obj=timerfd_and_match_mapping()
+
+    def __init__(self):
+
+
+
+        ...
+
+    # only special fd 
+    @classmethod
+    def fired_special_fd_handler(cls,fd,eventloop):
+
+
+        matches,state=getting_the_matches.get_matches(eventloop)
+
+
+        alert_handler.alert(matches)
+
+
+        
+
+
+    
+    # normal fd 
+    @classmethod
+    def fired_normal_fd_handler(cls,fd):
+
+        match=cls.mapping_obj[fd]
+
+        # delegating the sending to another object 
+
+        alert_handler.alert(match)
+    
+
+    @classmethod
+    def new_fd(cls,match,fire_time,interval):
+
+        # print(fire_time.split(":"))
+
+        hours,minuts=map(int,fire_time.split(":"))
+
+        target_time=time(hour=hours,minute=minuts)
+
+        complete_target_date=datetime.combine(datetime.now().date(),target_time)
+
+        delta=complete_target_date-datetime.now()
+
+        seconds_till_target=int(delta.total_seconds())
+
+        fd=timerfd.create_timerfd(seconds_till_target,interval)
+
+        cls.mapping_obj[fd]=match
+
+        return fd 
+
+    
